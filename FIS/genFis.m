8cdfb0cdd0b690f34c10d2921c5a4eb68a0e51ca
Aranyak-Ghosh <32264197+Aranyak-Ghosh@users.noreply.github.com>
2018-12-02 14:55:43 +0400

    Updated Script for generating FIS files

--- FIS/genFis.m
+++ FIS/genFis.m
@@ -2,16 +2,29 @@
 clear
 clc
 
+%Number of membership function for each input variable
+numberMembershipFunction=[4 4 4 4];
+
+%Type of membership function for each input
+inputMembershipFunctionType=["gbellmf" "gbellmf" "gbellmf" "gbellmf"];
+
+%Specify the type of membership function used for output
+outputMembershipFunctionType="constant";
+
+%Load the dataset used for generating FIS
 load TrainingSet.dat;
 
+%Specify the technique used for generating FIS
 opt=genfisOptions("GridPartition");
 
-opt.NumMembershipFunctions = [4 4 4 4];
+opt.NumMembershipFunctions = numberMembershipFunction;
 
-fis=genfis(TrainingSet(:,1:4),TrainingSet(:,5),opt)
+opt.InputMembershipFunctionType=inputMembershipFunctionType;
 
-writefis(fis,"GaussianMF");
+opt.OutputMembershipFunctionType=outputMembershipFunctionType;
+
+fis=genfis(TrainingSet(:,1:4),TrainingSet(:,5),opt);
+
+%Store the FIS generated in a file
+writefis(fis,"GBellMF");
 %%
-load TrainingSet.dat;
-
-fis=readfis("GeneratedFis.fis");
